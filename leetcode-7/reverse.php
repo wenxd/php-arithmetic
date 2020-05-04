d22c71888a47c7a3d40f0718a9ac076847933821
william <wenxiangduo@sunlands.com>
2020-05-04 19:07:10 +0800

    reverse

---
+++ leetcode-7/reverse.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+ * 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231,  231 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
+ */
+
+function reverse($x) {
+    if ($x >= 0) {
+        if (strrev($x) > (pow(2, 31) - 1)) {
+            return 0;
+        }
+        return (int)(strrev($x));
+    } else {
+        if (strrev(abs($x)) > pow(2, 31)) {
+            return 0;
+        }
+        return  '-' . (int)(strrev(abs($x)));
+    }
+}
